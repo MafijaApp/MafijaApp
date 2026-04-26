e4eec9afd5588741c8de0d95ff73563385484e4e
MafijaApp <markoveljovic2009@gmail.com>
2026-04-27 01:33:38 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,20 @@
-const CACHE_NAME = "mafija-cache-v1";
+const CACHE = "mafija-v3";
 
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/client.js"
-];
-
-// INSTALL
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/style.css",
+                "/client.js"
+            ]);
+        })
+    );
 });
 
-// FETCH
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
 });
